91aa9a4716390972ed99a9a1b351ac95756dfff6
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-02-08 01:18:52 -0800

    Add files via upload

---
+++ JavaSession8feb/practice/RegisterUser.java
@@ -0,0 +1,11 @@
+package JavaSession15;
+
+public class RegisterUser {
+    public void validate(int age,String citizenship) throws InvalidAgeException {
+        if(age <= 18)
+            throw new InvalidAgeException("You are Minor");//2options==>try catch /throws
+
+        if(!"Indian".equalsIgnoreCase(citizenship))
+            throw  new InvalidCitizenshipException("Citizenship should be INDIAN");
+    }
+}
