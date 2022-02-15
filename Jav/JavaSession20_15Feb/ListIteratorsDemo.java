5fc26564cdf7642cc03dcf626d33e5007759b7b2
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-02-15 01:36:32 -0800

    Update ListIteratorsDemo.java

--- Jav/JavaSession20_15Feb/ListIteratorsDemo.java
+++ Jav/JavaSession20_15Feb/ListIteratorsDemo.java
@@ -36,6 +36,16 @@ public class ListIteratorsDemo {
 
         while (i.hasPrevious())
             System.out.print(i.previous() + " ");
+        
+       
+        System.out.println();
+        System.out.println("First Element:"+list.firstElement());
+        System.out.println("Last Elements:"+list.lastElement());
+        list.set(1,"Utkarsh");//replace value 
+        System.out.println("After Replacing 1st index: ");
+        while(i.hasNext())
+            System.out.print(i.next()+" ");
+
 
 
     }
@@ -50,6 +60,13 @@ Forward Traversal---
 Gita Sita Gita null Rita Prita
 Backward Traversal---
 Prita Rita null Gita Sita Gita
+First Element:Gita
+Last Elements:Prita
+After Replacing 1st index: 
+Gita Utkarsh Gita null Rita Prita 
+Process finished with exit code 0
+
+
 
 //Difference between ListIterator and Iterator
 Iterators only retrieves the data in forward direction only ,where as ListIterator can retrive the data in forward as well as in backword direction .
