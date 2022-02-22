879c25a6c62d6b94b3f8e85000e3dbfb6956e6c2
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-02-22 17:07:26 +0530

    Create CarClass.java

---
+++ JavaSession25_jdk9/CarClass.java
@@ -0,0 +1,16 @@
+package car.main;
+public class CarClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Car");
+    }
+}
+
+/*
+Output:
+Hello Car
+
+
+// I have Created module Car in "JavaTraining" and have included "CarClass" in
+'src' of 'car' Module and have included the class in the package car.main
+
+ */
