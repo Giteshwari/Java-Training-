0d53c6d743c79aa52272b0a1ecc92bcba51b74b6
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-02-23 14:52:26 +0530

    Update MemoryPolluter.java

--- JavaSession26_jdkUpdates/MemoryPolluter.java
+++ JavaSession26_jdkUpdates/MemoryPolluter.java
@@ -23,4 +23,18 @@ Starting pollution
 Terminating
 
 
+// when  run the Application without Standard Vm option
+output:
+
+ Starting pollution
+    Terminating due to java.lang.OutOfMemoryError: Java heap space
+
+
+ As the code creates one-megabyte-arrays in a loop. Since we repeat the loop 10240 times,
+ it means we allocate 10 gigabytes of memory, which is probably higher than the available maximum heap size.
+
+Because  Once the available Java heap is exhausted, the JVM will shut down. and our Application terminates with outOfMemory Error.
+
+
+
  */
