d7c0c2ee90e7d7063f96963eb8dcb502a4012f9a
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-03-09 09:29:04 +0530

    Add files via upload

---
+++ SessionManagement/urlRewriting/src/FormServlet.java
@@ -0,0 +1,65 @@
+package com.gita;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/form")
+public class FormServlet extends HttpServlet
+{
+
+	@Override
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+	{
+
+		res.setContentType("text/html");
+
+		PrintWriter out=res.getWriter();
+
+
+		String uname = req.getParameter("user");
+		String password = req.getParameter("pass");
+		
+		
+
+		if(uname.equals("Gita") && password.equals("pass")) {
+			
+			
+			out.print("Welcome "+uname); 
+			int num1 = 0, num2 = 0;
+			
+			out.println("<form action=\"welcome\" method=\"get\">\r\n"
+					+ "\r\n"
+					+ "Enter number one :<input type=\"text\" name =\"num1\"><br><br>\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "Enter number two :<input type=\"text\" name=\"num2\"><br>\r\n"
+					+ "<br>\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "<input type=\"submit\" value=\"Enter\">\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "\r\n"
+					+ "</form>");
+			
+			out.print("<a href='welcome?aliasname="+uname+"'>Welcome </a>");
+			
+			
+		}
+		else {
+			out.println("Sorry !!Either of username or Password was incorrect");
+		}
+
+
+	}
+
+}
