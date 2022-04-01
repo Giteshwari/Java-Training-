f716559533879ded23e3664d4669c7b953241c50
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-04-01 17:46:44 +0530

    Update TeacherDao.java

--- Jpa_Teacher_Demo_updated/JPATeacherDemo/src/main/java/com/dao/TeacherDao.java
+++ Jpa_Teacher_Demo_updated/JPATeacherDemo/src/main/java/com/dao/TeacherDao.java
@@ -4,6 +4,7 @@ import com.bean.Teacher;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaUpdate;
 import javax.persistence.criteria.Root;
 import java.io.BufferedReader;
@@ -44,26 +45,16 @@ public class TeacherDao {
 
     }
 
-    public static void DeleteRecords() throws IOException {
-        em.setFlushMode(FlushModeType.COMMIT);
-        List<Teacher> teachers = em.createQuery("From Teacher",Teacher.class).getResultList();
+    public static void DeleteRecords(float sal) throws IOException {
 
         em.getTransaction().begin();
-        for(int i=0;i< teachers.size();i++){
-            Teacher teacher = teachers.get(i);
-            System.out.println("Enter the name to be updated");
-            teacher.setName(br.readLine());
-            teacher = em.merge(teacher);
-            em.persist(teacher);
-
-            int batchSize =2;
-            if(i% batchSize ==0 && i>0){
-                em.flush();
-                em.clear();
-            }
-
-        }
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaDelete<Teacher> delete = cb.createCriteriaDelete(Teacher.class);
+        Root e = delete.from(Teacher.class);
+        delete.where(cb.lessThan(e.get("salary"),sal));
+        em.createQuery(delete).executeUpdate();
         em.getTransaction().commit();
+        System.out.println("Delete Successful");
 
 
 
