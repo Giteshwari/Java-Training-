fd8356588c70266fd98a14b8c64f2fc6ad79674b
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-04-04 14:29:17 +0530

    Add files via upload

---
+++ SpringHelloWorld/HelloWorld/src/main/java/com/bean/MainApplication.java
@@ -0,0 +1,13 @@
+package com.bean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBean.xml");
+        HelloWorld object = (HelloWorld) context.getBean("Hello");
+        object.printHello();
+    }
+}
