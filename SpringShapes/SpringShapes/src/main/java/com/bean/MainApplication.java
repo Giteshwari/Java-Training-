2b843e148ad1d9bcd45833e294c683f2ebccc373
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-04-04 22:33:50 +0530

    Update MainApplication.java

--- SpringShapes/SpringShapes/src/main/java/com/bean/MainApplication.java
+++ SpringShapes/SpringShapes/src/main/java/com/bean/MainApplication.java
@@ -6,12 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Springbean.xml");
-        Shapes shape,shape1;
+        Shapes shape;
         shape = (Shapes) context.getBean("triangle");
         shape.draw();
 
-        shape1 = (Shapes) context.getBean("circle");
-        shape1.draw();
+       shape= (Shapes) context.getBean("circle");
+       shape.draw();
 
 
 
