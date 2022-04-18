a8c8a3547332ed851c0fb1308185b9c8cda6aeae
Giteshwari <70425831+Giteshwari@users.noreply.github.com>
2022-04-18 17:21:17 +0530

    Update CalculatorTest.java

--- Junit ca;culator with main method/JunitDemo/src/main/java/CalculatorTest.java
+++ Junit ca;culator with main method/JunitDemo/src/main/java/CalculatorTest.java
@@ -1,29 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-    Calculator obj_a = new Calculator();
+
     @Test
+    @After
     public void testAdd(){
+        Calculator obj_a = new Calculator();
         assertEquals(10,obj_a.add(5,5));
 
     }
+    
     @Test
     public void testDiv(){
         Division obj_d = new Division();
         assertEquals("Success",5,obj_d.div(10,2));
     }
+    
     @Test
+    @Before
     public void testMult(){
         Multiplication obj_m = new Multiplication();
         assertEquals("Multiplication done",15,obj_m.mult(5,3));
     }
+    
     @Test
+    @Ignore
     public void testSub(){
         Subtraction obj_s = new Subtraction();
-        assertEquals(2,obj_s.sub(6,4));
-
+        assertEquals("2",obj_s.sub(6,4));
     }
 
 
